6aecca2c9bd01906276bbdfb5283ad9770154b6f
Henry Chen <henrychen385@gmail.com>
2024-03-18 23:16:19 +0800

    added splicing feature

--- Stitcher.py
+++ Stitcher.py
@@ -1,21 +1,44 @@
 import random
 from pydub import AudioSegment
+from Snippet import Snippet
+from Song import Song
 
 class Stitcher():
     def __init__(self, song_list, snippet_list, ad_list):
         self.song_list = song_list
         self.snippet_list = snippet_list
         self.ad_list = ad_list
+        self.inserts = [*snippet_list, *ad_list]
 
     def assemble(self):
+        random.shuffle(self.inserts)
 
-        inserts = [*self.snippet_list, *self.ad_list][:len(self.song_list)]
+        assembly = AudioSegment.silent(duration=0)
 
-        assembly = AudioSegment.silent(duration=100)
-
-        for song, insert in zip(self.song_list, inserts):
-            print(song.filepath)
-            print(insert.filepath)
+        for song, insert in zip(self.song_list, self.inserts[:len(self.song_list)]):
             assembly += insert.audio() + song.audio()
 
         return assembly
+
+class SplicedSong(Song):
+    def __init__(self, filepath, fade_in_sec=None, fade_out_sec=None):
+        super().__init__(filepath, fade_in_sec, fade_out_sec)
+        self.snippet_time_pairs = None
+
+    def splice_in_at_time(self, snippet_filepath, splice_time_sec):
+        if (not self.snippet_time_pairs):
+            self.snippet_time_pairs = [(splice_time_sec, snippet_filepath)]
+        else:
+            self.snippet_time_pairs.append((splice_time_sec, snippet_filepath))
+
+    def audio(self):
+        orig = self.wav
+        reversed_snippets = sorted(self.snippet_time_pairs, key=lambda x: x[0], reverse=True)
+        out = orig
+        for time_sec, path in reversed_snippets:
+            snippet = Snippet(path)
+            orig_head = out[:time_sec*1000]
+            orig_tail = out[time_sec*1000:]
+            out = orig_head.fade_out(30) + snippet.audio() + orig_tail.fade_in(30)
+
+        return out
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pydub.playback import play
 from Snippet import Snippet
 from Song import Song
 from Ad import Ad
-from Stitcher import Stitcher
+from Stitcher import Stitcher, SplicedSong
 
 def main():
     print("Hello world...")
@@ -13,19 +13,25 @@ def main():
     ad1 = Ad("snippets/alpha.mp3")
     ad2 = Ad("snippets/ram4.mp3")
 
-    song1 = Song("snippets/songs/song1.mp3")
-    song2 = Song("snippets/songs/song2.mp3")
+    song1 = SplicedSong("snippets/songs/wii.mp3")
+    song1.splice_in_at_time("snippets/alpha.mp3", 10)
+    song1.splice_in_at_time("snippets/keep_running.mp4", 20)
+
+    # song2 = Song("snippets/songs/confused.mp3")
+    # song3 = Song("snippets/songs/scheming.mp3")
+    # song4 = Song("snippets/songs/song2.mp3")
 
     print("Building song...")
     stitch = Stitcher(
-        song_list = [song1, song2],
+        song_list = [song1],
         snippet_list = [track1, track2],
-        ad_list = [song1, song2]
+        ad_list = [ad1, ad2]
     )
 
     out = stitch.assemble()
 
-    out.export("build/build.mp3", format="mp3")
+    # out.export("build/build.mp3", format="mp3")
+    play(out)
 
 if __name__ == "__main__":
     main()
